6d37677d9abb87b611fac403be239aab2a77a878
Alexandre Angrignon <alex040892@gmail.com>
2024-04-06 14:44:01 +1300

    Router Setup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 import express from "express";
 import colors from "colors";
 import db from "./config/Database.js";
-import Users from "./models/UserModel.js";
+// import Users from "./models/UserModel.js";
+import router from "./routes/index.js";
+
 const app = express();
 
+// Db Connection
 try {
   await db.authenticate();
   console.log(colors.magenta("Database connected...."));
-  await Users.sync();
+  //   await Users.sync();
 } catch (error) {
   console.error(error);
 }
+
+//Router Connection
+app.use(express.json());
+app.use(router);
 
 app.listen(5000, () =>
   console.log(colors.cyan("Server is running at port 5000"))
